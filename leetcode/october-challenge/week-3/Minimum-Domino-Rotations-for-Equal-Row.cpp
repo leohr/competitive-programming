487a07843a53d8dbb72f5904e27ada5151dc36e8
leoHuerta <leonel.huerta.retamal@gmail.com>
2020-10-19 23:48:02 -0300

    Add solution

---
+++ leetcode/october-challenge/week-3/Minimum-Domino-Rotations-for-Equal-Row.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minDominoRotations(vector<int>& A, vector<int>& B) {
+        int ans = 1e5; 
+        for (int x = 1; x <= 6; ++x) {
+            int Amin = 0, Bmin = 0; 
+            bool bad = false; 
+            for (int i = 0; i < A.size(); ++i) {
+                if (A[i] != x && B[i] != x) {
+                    Amin = -1;
+                    Bmin = -1; 
+                    bad = true; 
+                    break; 
+                }
+                else if (A[i] == x && B[i] != x) ++Bmin; 
+                else if (A[i] != x && B[i] == x) ++Amin; 
+            }
+            if (!bad)
+                ans = min(ans, min(Amin, Bmin)); 
+        }        
+        if (ans < 1e5)
+            return ans; 
+        else 
+            return -1; 
+    }
+};
