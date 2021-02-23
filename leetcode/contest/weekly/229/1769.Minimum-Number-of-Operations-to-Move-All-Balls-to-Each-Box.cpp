64d019ef0655aa2a698aada00f3254830dbca91d
leoHuerta <leonel.huerta.retamal@gmail.com>
2021-02-23 20:21:52 -0300

    Add solution

---
+++ leetcode/contest/weekly/229/1769.Minimum-Number-of-Operations-to-Move-All-Balls-to-Each-Box.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        int n = boxes.size(); 
+        vector<int> ans(n); 
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (boxes[j] == '1')
+                    ans[i] += abs(j - i);
+            }
+        }
+        return ans; 
+    }
+};
