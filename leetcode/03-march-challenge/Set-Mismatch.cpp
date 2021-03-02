16c658916fe6892bde307cdcb4029d65a965d37f
leoHuerta <leonel.huerta.retamal@gmail.com>
2021-03-02 11:13:55 -0300

    Add solution

---
+++ leetcode/03-march-challenge/Set-Mismatch.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(2); 
+        sort(nums.begin(), nums.end());
+        map<int, int> mp; 
+        for (int i = 0; i < n; ++i) {
+            ++mp[nums[i]]; 
+        }
+        for (int i = 1; i <= n; ++i) {
+            if (mp[i] == 2) 
+                ans[0] = i; 
+            if (mp[i] == 0) 
+                ans[1] = i; 
+        }
+        return ans; 
+    }
+};
