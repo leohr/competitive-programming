456c44fecac3c61ec09d4537b81159ca21cf2af7
Leo Huerta <leonel.huerta.retamal@gmail.com>
2021-06-13 00:17:57 -0400

    Add solution

---
+++ leetcode/arrays101/Duplicate-zeros.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int n = arr.size(); 
+        for (int i = 0; i < n; ++i) {
+            if (arr[i] == 0) {
+                arr.insert(arr.begin() + i, 0);
+                ++i; 
+            }
+        }
+        arr.erase(arr.begin()+n, arr.end()); 
+    }
+};
