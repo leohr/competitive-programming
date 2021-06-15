2b9f11cc6b97a83f4c2e5f1b08bbcbcd7839474d
Leo Huerta <leonel.huerta.retamal@gmail.com>
2021-06-15 00:28:26 -0400

    Add solution

---
+++ leetcode/arrays101/Check-If-N-and-Its-Double-Exist.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        int n = arr.size(); 
+        for (int i = 0; i < n; ++i) {
+            for (int j = i+1; j < n; ++j) {
+                if (arr[i] == 2*arr[j] || arr[j] == 2*arr[i]) 
+                    return true; 
+            }
+        }
+        return false; 
+    }
+};
