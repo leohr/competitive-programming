99c1a95b0d64c58d97b5cd9eb60b268d33d82a0c
leohr <70021575+leohr@users.noreply.github.com>
2022-11-09 17:19:19 -0300

    Update 09.Online-Stock-Span.py

--- leetcode/2022/11.november/09.Online-Stock-Span.py
+++ leetcode/2022/11.november/09.Online-Stock-Span.py
@@ -6,11 +6,11 @@ class StockSpanner:
 
     def next(self, price: int) -> int:
         self.flow.append(price)
-        
-        if (len(self.flow) == 1): 
+        j = len(self.flow) - 1
+
+        if (j == 0): 
             return 1
             
-        j = len(self.flow) - 1
         if (self.flow[ j ] < self.flow[ j - 1 ]):
             self.i = j - 1
             
@@ -20,9 +20,8 @@ class StockSpanner:
             else: 
                 break 
 
-        return j - self.i 
-      
-      
+        return j - self.i  
+
 # Your StockSpanner object will be instantiated and called as such:
 # obj = StockSpanner()
 # param_1 = obj.next(price)
