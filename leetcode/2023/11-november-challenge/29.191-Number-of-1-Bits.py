3523bccfd43d776c7966d1206fdedabe30a0e43c
leohr <70021575+leohr@users.noreply.github.com>
2023-11-29 11:38:47 -0300

    Create 29.191-Number-of-1-Bits.py

---
+++ leetcode/2023/11-november-challenge/29.191-Number-of-1-Bits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        ans = 0 
+        while n > 0: 
+            if n % 2 == 1: 
+                ans += 1 
+            n = int(n/2) 
+        return ans
